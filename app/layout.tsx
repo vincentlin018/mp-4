578e2872e2a57b8c1b8fabeb9a714067fa175a06
Vincent Lin <vinvlin@bu.edu>
2025-04-04 00:09:46 -0400

    seeing if this fix works on Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,33 +2,36 @@
 import styled from "styled-components";
 import React from 'react';
 import Header from './components/Header';
+import { StyledEngineProvider } from '@mui/material/styles';
 
 const StyledBody = styled.div`
-    width: 80%;
-    margin: auto;
-    background-color: lightgoldenrodyellow;
-    min-height: 100vh;
+  width: 80%;
+  margin: auto;
+  background-color: lightgoldenrodyellow;
+  min-height: 100vh;
 `;
 
 const StyledChild = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin: auto;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
 `;
 
 export default function RootLayout(
     {children,}:
-      Readonly<{ children: React.ReactNode; }>) {
+    Readonly<{ children: React.ReactNode; }>) {
   return (
-    <html lang="en">
-    <body>
+      <html lang="en">
+      <body>
       <StyledBody>
-        <Header />
-        <StyledChild>
-          {children}
-        </StyledChild>
+        <StyledEngineProvider injectFirst>
+          <Header />
+          <StyledChild>
+            {children}
+          </StyledChild>
+        </StyledEngineProvider>
       </StyledBody>
-    </body>
-    </html>
+      </body>
+      </html>
   );
 }
